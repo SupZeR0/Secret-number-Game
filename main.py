d334f02ac76848fdf056871e395412d16e8ba70d
Abdo Shaker <23082576-AbdoShaker1@users.noreply.replit.com>
2025-03-25 23:00:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+def greet_user(name ,company):
+  print("\n"f"Welcome {name}  at {company} Python Course")
+  print("Enjoy youre learning journey")
+  
+greet_user(input("Please enter your name: " )) 
